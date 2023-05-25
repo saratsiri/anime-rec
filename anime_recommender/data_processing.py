17667aed35108a6aca08d5f2b890d30d53ca11f5
Sarat Sirirachatapong <sarat.fin@gmail.com>
2023-05-25 14:25:34 +0900

    preliminary push so I can research my laptop

--- anime_recommender/data_processing.py
+++ anime_recommender/data_processing.py
@@ -1,24 +1,25 @@
-import pandas as pd
-
+import pandas as pd #required pandas == 1.4.4
+import os
+print(pd.__version__)
 # Import datasets
-UsersDF = pd.read_csv('../raw_data/users_cleaned.csv')
-AnimesDF = pd.read_csv('../raw_data/anime_cleaned.csv')
-ScoresDF = pd.read_csv('../raw_data/animelists_cleaned.csv')
-
+# print("Loading data...\n")
+# UsersDF = pd.read_csv(f"{os.getcwd()}/raw_data/users_cleaned.csv")
+# AnimesDF = pd.read_csv(f"{os.getcwd()}/raw_data/anime_cleaned.csv")
+# ScoresDF = pd.read_csv(f"{os.getcwd()}/raw_data/animelists_cleaned.csv")
+# print("Loading data completed\n")
 # Trim ScoresDF to essential columns
 ScoresDF = ScoresDF[['username', 'anime_id', 'my_score', 'my_status']]
 
 # Find and print the range of ratings
 lower_rating = ScoresDF['my_score'].min()
 upper_rating = ScoresDF['my_score'].max()
-print('Range of ratings vary between: {0} to {1}'.format(lower_rating, upper_rating))
 
 # Generate a dataframe detailing number of animes rated by each user
 user_anime_counts = ScoresDF['username'].value_counts()
 UsersAndScores = pd.DataFrame({'username': user_anime_counts.index, 'animes_rated': user_anime_counts.values})
 
 # Merge sampled user data with UsersAndScores
-UsersSampled = UsersDF.sample(frac = 1) # Full sampling
+UsersSampled = UsersDF.sample(frac = .01) # Full sampling
 UsersAndScoresSampled = pd.merge(UsersAndScores, UsersSampled, on = 'username')
 
 # Group users by the same number of animes rated
@@ -44,4 +45,5 @@ ScoresDFHotStart = pd.merge(ScoresDF, UserRatedsCutten, on = 'username')
 ScoresDFHotStart = pd.merge(ScoresDFHotStart, AnimeRatedsCutten, on = 'anime_id')
 
 # Save ScoresDFHotStart to a CSV file
-ScoresDFHotStart.to_csv('../processed_data/ScoresDFHotStart.csv', index=False)
+ScoresDFHotStart.to_csv(f"{os.getcwd()}/processed_data/ScoresDFHotStart", index = False)
+print("Data processing completed.")
--- anime_recommender/training.py
+++ anime_recommender/training.py
@@ -3,10 +3,12 @@ from collections import defaultdict
 from scipy.sparse.linalg import svds
 import surprise as sp
 import pickle
+import os
 
+print("Loading data...\n")
 # Load preprocessed data
 ScoresDFHotStart = pd.read_csv('../processed_data/ScoresDFHotStart.csv')
-
+print("Loading data completed\n")
 # Initialize reader and load data into Surprise dataset format
 reader = sp.Reader(rating_scale=(0, 10))
 data = sp.Dataset.load_from_df(ScoresDFHotStart[['username', 'anime_id', 'my_score']], reader)
@@ -24,19 +26,24 @@ baseline_model.fit(trainset_full)
 
 # Generate anti-testset and make predictions
 anti_testset = trainset_full.build_anti_testset()
+print("Training Baseline Model...\n")
 predictions = baseline_model.test(anti_testset)
+print("Baseline Model Completed\n")
 
 # Convert predictions to dataframe and drop unnecessary column
 predictions_df = pd.DataFrame(predictions, columns=['uid', 'iid', 'rui', 'est', 'details'])
 predictions_df.drop('rui', axis=1, inplace=True)
 
 # Train a KNNBaseline model
+print("Training KNN model\n")
 sim_options = {'name': 'pearson_baseline', 'user_based': False}
 knn_model = sp.KNNBaseline(sim_options=sim_options)
 knn_model.fit(trainset_full)
-
+print("KNN Model Completed")
 # Save models to pickle files
-with open('baseline_model.pickle', 'wb') as f:
+print("Saving..\n")
+with open(f"{os.getcwd()}/trained_models/baseline_model.pickle", 'wb') as f:
     pickle.dump(baseline_model, f)
-with open('knn_model.pickle', 'wb') as f:
+with open(f"{os.getcwd()}/trained_models/knn_model.pickle", 'wb') as f:
     pickle.dump(knn_model, f)
+print("Saving Completed\n")
